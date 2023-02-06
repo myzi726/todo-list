ff7043d596c18f182c905516790eca32498d808d
myzi726 <mzi726@naver.com>
2023-02-07 02:06:23 +0900

    initial commit/TodoList

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+
+class App extends Component {
+  render() {
+    return (
+      <TodoListTemplate form={<Form/>}>
+      </TodoListTemplate>
+    );
+  }
+}
+
+export default App;
