c28f1cad18f96109f707f69e564c82afb7f1c242
myzi726 <mzi726@naver.com>
2023-02-08 23:56:06 +0900

    feat: todo checked, remove

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import './TodoItem.css';
 
 class TodoItem extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.checked !== nextProps.todos;
+    }
+
     render() {
         const { text, checked, id, onToggle, onRemove} = this.props;
 
